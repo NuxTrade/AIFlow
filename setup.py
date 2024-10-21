f6cb3b17a3050e70b181a2e1b51a62d0004ee93b
1105398+ma2bd <1105398+ma2bd@users.noreply.github.com>
2024-10-21 23:21:07 +0800

    Version update of release to 0.0.3 Programm

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiflow",                          
-    version="0.0.3-rc1",                    
+    version="0.0.3",                    
     author="NuxTrade",                       
     description="Create composable AI agents",
     long_description=long_description,      
